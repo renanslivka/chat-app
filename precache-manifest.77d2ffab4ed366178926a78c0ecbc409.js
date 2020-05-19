1d0434e0d69bb7c444b642bc1657783832b84e89
renanslivka <renanslivka@gmail.com>
2020-05-19 16:20:32 +0300

    Updates

---
+++ precache-manifest.77d2ffab4ed366178926a78c0ecbc409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352acf454a835909a0aa509afd8021a6",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "ecebeb5d3bcaace96fad",
+    "url": "/chat-app/static/css/main.f0b82199.chunk.css"
+  },
+  {
+    "revision": "9fa493c93b0f0abdb98d",
+    "url": "/chat-app/static/js/2.5cac8af0.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/chat-app/static/js/2.5cac8af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecebeb5d3bcaace96fad",
+    "url": "/chat-app/static/js/main.b0c96e84.chunk.js"
+  },
+  {
+    "revision": "f5d7b11ef027c12da41d",
+    "url": "/chat-app/static/js/runtime-main.4888d7ac.js"
+  }
+]);
